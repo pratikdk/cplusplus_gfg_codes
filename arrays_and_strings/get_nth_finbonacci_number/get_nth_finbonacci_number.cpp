108dd59ae1689c76d39db8c44738fd022673547b
unknown <pratik.deoolwadikar@gmail.com>
2020-05-08 01:37:53 +0530

    Daily code: language drill

--- arrays_and_strings/get_nth_finbonacci_number/get_nth_finbonacci_number.cpp
+++ arrays_and_strings/get_nth_finbonacci_number/get_nth_finbonacci_number.cpp
@@ -2,10 +2,32 @@
 
 using namespace std;
 
-int[] generate_fibonacci_sequence(int sequence_limit) {
+int* generate_fibonacci_sequence(int sequence_limit) {
+
+    int *fib_arr = new int[sequence_limit];
+    // Seed values
+    fib_arr[0] = 0;
+    fib_arr[1] = 1;
     
+    for (int i = 2; i < sequence_limit; i++) {
+        fib_arr[i] = fib_arr[i-1] + fib_arr[i-2];
+    }
+
+    return fib_arr;
 }
 
 int main() {
+    int sequence_limit = 50;
 
+    int *fibarr = generate_fibonacci_sequence(sequence_limit);
+
+    for (int i = 0; i < sequence_limit; i++) {
+        cout << *(fibarr + i) << " ";
+    }
+
+    // get nth fibonacci number
+    int n = 9;
+    cout << "\n" << *(fibarr + n) << "\n";
+
+    return 0;
 }
