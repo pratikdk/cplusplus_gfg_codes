70ecf67cb40652214e75799093b35fee7fcec3f8
unknown <pratik.deoolwadikar@gmail.com>
2020-05-16 14:58:00 +0530

    Daily code: language drill

---
+++ pointers_and_objects/subscripting_pointer_to_array/subscripting_pointer_to_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int arr[3][4] = {  
+        {10, 11, 12, 13},  
+        {20, 21, 22, 23},  
+        {30, 31, 32, 33}  
+    }; 
+
+    // Pointer to an entire array
+    int (*ptr)[4];
+    ptr = arr;
+
+    printf("%p %p %p\n", ptr, ptr+1, ptr+2 );
+    printf("%p %p %p\n", *ptr, *(ptr + 1), *(ptr + 2) );
+    printf("%d %d %d\n", *(*ptr), *(*(ptr+1)+2), *(*(ptr+2)+3) );
+    printf("%d %d %d\n", ptr[0][0], ptr[1][2], ptr[2][3] );
+
+    return 0;
+}
